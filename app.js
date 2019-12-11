bb16119a30c6a39063e8ee8955bc391f62c037d7
German <german1728@gmail.com>
2019-12-10 20:28:19 -0500

    uff

--- app.js
+++ app.js
@@ -12,17 +12,17 @@ const cameraView = document.querySelector("#camera--view"),
 function cameraStart() {
     navigator.mediaDevices
         .getUserMedia(constraints)
-        .then(function(stream) {
+        .then(function (stream) {
             track = stream.getTracks()[0];
             cameraView.srcObject = stream;
         })
-        .catch(function(error) {
+        .catch(function (error) {
             console.error("Oops. Something is broken.", error);
         });
 }
 
 // Take a picture when cameraTrigger is tapped
-cameraTrigger.onclick = function() {
+cameraTrigger.onclick = function () {
     cameraSensor.width = cameraView.videoWidth;
     cameraSensor.height = cameraView.videoHeight;
     cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
@@ -31,5 +31,58 @@ cameraTrigger.onclick = function() {
     // track.stop();
 };
 
+var doUpload = function (event) {
+
+    var input = event.target;
+    var reader = new FileReader();
+
+
+    reader.onload = function () {
+        var arrayBuffer = reader.result;
+        var arrayBufferView = new Uint8Array(arrayBuffer);
+        var blob = new Blob([arrayBufferView], { type: input.files[0].type });
+        var urlCreator = window.URL || window.webkitURL;
+        var imageUrl = urlCreator.createObjectURL(blob);
+        $.ajax({
+            url: 'https://content.dropboxapi.com/2/files/upload',
+            type: 'post',
+            data: file,
+            processData: false,
+            contentType: 'application/octet-stream',
+            headers: {
+                "Authorization": "Bearer 5YL9E1Q3xRcAAAAAAAAAlNx76-C6rKBejpxV4z1AqOxoPIeMdCBADx3aR09exTKO",
+                "Dropbox-API-Arg": '{"path": "' + imageUrl + ',"mode": "add","autorename": true,"mute": false}'
+            },
+            success: function (data) {
+                console.log(data);
+            },
+            error: function (data) {
+                console.error(data);
+            }
+        })
+
+    }
+    reader.readAsArrayBuffer(input.files[0]);
+}
+
 // Start the video stream when the window loads
 window.addEventListener("load", cameraStart, false);
+/*
+file = event.target.files[0];
+$.ajax({
+    url: 'https://content.dropboxapi.com/2/files/upload',
+    type: 'post',
+    data: file,
+    processData: false,
+    contentType: 'application/octet-stream',
+    headers: {
+        "Authorization": "Bearer <ACCESS_TOKEN>",
+        "Dropbox-API-Arg": '{"path": "/test_upload.txt","mode": "add","autorename": true,"mute": false}'
+    },
+    success: function (data) {
+        console.log(data);
+    },
+    error: function (data) {
+        console.error(data);
+    }
+})*/
